a23f5c5378a014c38824883abaad471d9cae015e
Gordon Ho <GordonPrivate1234567890@protonmail.com>
2023-10-17 16:04:40 -0600

    Αdded drive controls

--- alexgoesinsance/src/main.cpp
+++ alexgoesinsance/src/main.cpp
@@ -86,18 +86,23 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	pros::Motor left_mtr_1(20);
+	pros::Motor left_mtr_2(19);
+	pros::Motor_Group left_motors({left_mtr_1,left_mtr_2});
+
+	pros::Motor right_mtr_1(11);
+	pros::Motor right_mtr_2(12);
+	pros::Motor_Group right_motors({right_mtr_1,right_mtr_2});
+
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
+
+		left_motors.move(master.get_analog(ANALOG_LEFT_Y));
+		right_motors.move(master.get_analog(ANALOG_RIGHT_Y));
 
 		pros::delay(20);
 	}
