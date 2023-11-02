e98ffe959fc8beefb8d030208f575c041f6e7171
Kyle Wang <kylew85@educbe.ca>
2023-11-02 16:35:51 -0600

    modified:   alexgoesinsance/src/main.cpp removed a single 's'...

--- alexgoesinsance/src/main.cpp
+++ alexgoesinsance/src/main.cpp
@@ -19,7 +19,7 @@ commands:
 //Test
 void on_center_button() {
 	static bool pressed = false;
-	pressed = !pressed;s
+	pressed = !pressed;
 	if (pressed) {
 		pros::lcd::set_text(2, "I was pressed!");
 	} else {
