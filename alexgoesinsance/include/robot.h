708e269911dbb6e89cdc68413951f9a740a6bb11
Gordon Ho <GordonPrivate1234567890@protonmail.com>
2024-01-16 16:37:43 -0700

    a

--- alexgoesinsance/include/robot.h
+++ alexgoesinsance/include/robot.h
@@ -5,7 +5,7 @@
 #include "robot.h"
 
 #define MOTOR_RIGHT_TURN 90
-#define CATAPULT_INITIAL_SPIN 165*3
+#define CATAPULT_INITIAL_SPIN 82*3
 #define CATAPULT_LAUNCH_SPIN 15*3
 //wings
 void setWings(bool state);
